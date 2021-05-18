1d023256a676ffd263ba6c4cc33188d6ad917dae
Clara Mendes <claraplanejamento@gmail.com>
2021-05-17 21:28:35 -0300

    :lipstick: adds custom fonts and home page

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,14 @@
+import {Wrapper} from './styles'
+import './styles.css'
+
+function Home() {
+  return (
+    <Wrapper>
+      <h1>Clarafolio</h1>
+      <p>Hello, welcome to my humble portfolio. </p>
+      <p>I hope you enjoy it!</p>
+    </Wrapper>
+  );
+}
+
+export default Home;
---
+++ src/pages/Home/styles.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.div`
+    background-color: #fffdd0;
+    width: 100%;
+    height: 100vh;
+    font-family: Chomsky;
+`
